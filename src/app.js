3a7ec1a77df3ce8c88ac7f3e88687be68a5cb434
Christina Yin <yinchristina373@gmail.com>
2022-04-11 15:10:24 -0700

    created a note taking area and delete button to clear the area

--- src/app.js
+++ src/app.js
@@ -5,3 +5,36 @@ const notes = [
     id: 1 
   }
 ]
+
+const template = `
+<textarea id = 'noteSpace' rows="50" cols="100">Note</textarea>
+<button class="Save">Save</button>
+<button class="Delete">Delete</button>
+`
+
+function emptyNote(temp){
+  const noteArea = document.querySelector('.write-note-area')
+  noteArea.innerHTML = temp
+}
+
+function clearArea(){
+  const areaToClear = document.querySelector('.write-note-area')
+  areaToClear.innerHTML = ''
+}
+
+function createNote (){
+  emptyNote(template)
+  deleteBtn = document.querySelector('.Delete')
+  deleteBtn.addEventListener('click', clearArea)
+}
+
+function enableNewNote(){
+  const newNoteBtn = document.querySelector('.icons')
+  newNoteBtn.addEventListener('click', createNote)
+}
+
+function initialize(){
+  enableNewNote()
+}
+
+initialize()
